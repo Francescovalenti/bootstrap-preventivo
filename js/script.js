3197ae9bcdff56401c7a8a9db4c92f5e7a43ecd2
francescovalenti <francescovalenti83@outlook.it>
2025-03-11 19:52:30 +0100

    feat:inserimento dati function

--- js/script.js
+++ js/script.js
@@ -11,7 +11,10 @@ const sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 nome.addEventListener ("submit", function (event){
     event.preventDefault();
     console.log(submit);
-    
+    const Tipodilavoroinput = Tipodilavoroinput.value
+    const CodicePromozionaleinput = CodicePromozionaleinput.value
+
+    const orelavoro = 10 
 
 
     
