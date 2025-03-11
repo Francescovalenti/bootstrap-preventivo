b62e1f2175af209283f79c6113073cf0c68d49eb
francescovalenti <francescovalenti83@outlook.it>
2025-03-11 16:41:03 +0100

    feat:inizio javascript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Raccolta dati
+const nome = document.getElementById ("Nome");
+const inputcognome = document.getElementById ("cognomeinput");
+const inputEmail = document.getElementById ("emailinput");
+const inputTipodilavoro = document.getElementById ("Tipodilavoroinput");
+const exampleDescrizioneLavoro = document.getElementById ("exampleDescrizioneLavoroinput");
+const inputCodicePromozionale = document.getElementById ("CodicePromozionaleinput")
+
+const sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+
+nome.addEventListener ("submit", function (event){
+    event.preventDefault();
+    console.log(submit);
+    
+
+
+    
+})
+
+
