84312ab97f360d9ca5520169b00d7b8db8eceb5c
francescovalenti <francescovalenti83@outlook.it>
2025-03-12 10:54:15 +0100

    feat : inserimento sconti

--- js/script.js
+++ js/script.js
@@ -9,39 +9,36 @@ const CodicePromozionaleinput = document.getElementById ("CodicePromozionale");
 
 const sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 
-datiutenti.addEventListener ("submit", function (event){
+datiutentiinput.addEventListener ("submit", function (event){
     event.preventDefault();
     console.log("submit");
     const TipodiLavoro= TipodiLavoroinput.value;
     console.log(TipodiLavoro);
     
-    const CodicePromozionale = CodicePromozionaleinput.value
+    const CodicePromozionale = CodicePromozionaleinput.value;
     console.log(CodicePromozionale);
     
 
     const oreLavoro = 10 
-    const prezzoOrario = [20.50, 15.30, 33.60]
-   
-    for (let i = 0; i <= prezzoOrario.length; i++){
-        const inpprezzoOrario = prezzoOrario [i];
-        console.log(inpprezzoOrario);
-        
+    let prezzoOrario = ("20.50","15.30","33.60")
+     
     
     if (TipodiLavoro === "Backed Development")
-         {
-        inpprezzoOrario = 20.50;
-    } else if (TipodiLavoro === "Frontend Development ") {
-        inpprezzoOrario = 15.30;
+         { 
+        prezzoOrario = 20.50;
+    } else if (TipodiLavoro === "Frontend Development") {
+        prezzoOrario = 15.30;
     } else if(TipodiLavoro === "Project Analysis") {
-        inpprezzoOrario = 33.60;
+        prezzoOrario = 33.60;
     }
+
     
-    let prezzoFinale =(Math.round (oreLavoro * inpprezzoOrario)).toFixed(2);
+    let prezzoFinale =(Math.round (oreLavoro * prezzoOrario)).toFixed(2);
 
    
 
     console.log(prezzoFinale);
-}
+
 
 
     
