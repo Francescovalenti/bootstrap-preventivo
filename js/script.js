79eae762bf3ff9f314e933712156ee505b547078
francescovalenti <francescovalenti83@outlook.it>
2025-03-12 10:26:18 +0100

    feat: manca comparsa prezzo

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
 // Raccolta dati
+const datiutentiinput = document.getElementById("datiutenti");
 const nome = document.getElementById ("Nome");
 const inputcognome = document.getElementById ("cognomeinput");
 const inputEmail = document.getElementById ("emailinput");
-const inputTipodilavoro = document.getElementById ("Tipodilavoroinput");
+const TipodiLavoroinput = document.getElementById ("TipodiLavoro");
 const exampleDescrizioneLavoro = document.getElementById ("exampleDescrizioneLavoroinput");
-const inputCodicePromozionale = document.getElementById ("CodicePromozionaleinput")
+const CodicePromozionaleinput = document.getElementById ("CodicePromozionale");
 
 const sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 
-nome.addEventListener ("submit", function (event){
+datiutenti.addEventListener ("submit", function (event){
     event.preventDefault();
-    console.log(submit);
-    const Tipodilavoroinput = Tipodilavoroinput.value
-    const CodicePromozionaleinput = CodicePromozionaleinput.value
+    console.log("submit");
+    const TipodiLavoro= TipodiLavoroinput.value;
+    console.log(TipodiLavoro);
+    
+    const CodicePromozionale = CodicePromozionaleinput.value
+    console.log(CodicePromozionale);
+    
 
-    const orelavoro = 10 
+    const oreLavoro = 10 
+    const prezzoOrario = [20.50, 15.30, 33.60]
+   
+    for (let i = 0; i <= prezzoOrario.length; i++){
+        const inpprezzoOrario = prezzoOrario [i];
+        console.log(inpprezzoOrario);
+        
+    
+    if (TipodiLavoro === "Backed Development")
+         {
+        inpprezzoOrario = 20.50;
+    } else if (TipodiLavoro === "Frontend Development ") {
+        inpprezzoOrario = 15.30;
+    } else if(TipodiLavoro === "Project Analysis") {
+        inpprezzoOrario = 33.60;
+    }
+    
+    let prezzoFinale =(Math.round (oreLavoro * inpprezzoOrario)).toFixed(2);
+
+   
+
+    console.log(prezzoFinale);
+}
 
 
     
