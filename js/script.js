5c67f88f04e1a1e731d40ace967fb15c2b788360
francescovalenti <francescovalenti83@outlook.it>
2025-03-12 11:14:06 +0100

    feat : inserimento sconti

--- js/script.js
+++ js/script.js
@@ -1,47 +1,45 @@
 // Raccolta dati
 const datiutentiinput = document.getElementById("datiutenti");
-const nome = document.getElementById ("Nome");
-const inputcognome = document.getElementById ("cognomeinput");
-const inputEmail = document.getElementById ("emailinput");
-const TipodiLavoroinput = document.getElementById ("TipodiLavoro");
-const exampleDescrizioneLavoro = document.getElementById ("exampleDescrizioneLavoroinput");
-const CodicePromozionaleinput = document.getElementById ("CodicePromozionale");
+const nome = document.getElementById("Nome");
+const inputcognome = document.getElementById("cognomeinput");
+const inputEmail = document.getElementById("emailinput");
+const TipodiLavoroinput = document.getElementById("TipodiLavoro");
+const exampleDescrizioneLavoro = document.getElementById("exampleDescrizioneLavoroinput");
+const CodicePromozionaleinput = document.getElementById("CodicePromozionale");
 
 const sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 
-datiutentiinput.addEventListener ("submit", function (event){
+datiutentiinput.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log("submit");
-    const TipodiLavoro= TipodiLavoroinput.value;
-    console.log(TipodiLavoro);
-    
+    const TipodiLavoro = TipodiLavoroinput.value;
+
+
     const CodicePromozionale = CodicePromozionaleinput.value;
     console.log(CodicePromozionale);
-    
 
-    const oreLavoro = 10 
-    let prezzoOrario = ("20.50","15.30","33.60")
-     
-    
-    if (TipodiLavoro === "Backed Development")
-         { 
+
+    const oreLavoro = 10;
+    let prezzoOrario = 0;
+
+
+    if (TipodiLavoro === "Backed Development") {
         prezzoOrario = 20.50;
     } else if (TipodiLavoro === "Frontend Development") {
         prezzoOrario = 15.30;
-    } else if(TipodiLavoro === "Project Analysis") {
+    } else if (TipodiLavoro === "Project Analysis") {
         prezzoOrario = 33.60;
     }
+  
 
-    
-    let prezzoFinale =(Math.round (oreLavoro * prezzoOrario)).toFixed(2);
+    const sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 
-   
-
+    let prezzoFinale = (Math.round(oreLavoro * prezzoOrario)).toFixed(2);
     console.log(prezzoFinale);
 
 
 
-    
+
 })
 
 
